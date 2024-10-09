ae6efd6d98cf2d6048b1c03693208a6d262394e7
Nzola Kiampava <nzolakiampava@gmail.com>
2024-10-09 11:51:11 +0100

    Update generate_fractal.c

--- generate_fractal.c
+++ generate_fractal.c
@@ -12,54 +12,66 @@
 
 #include "fractol.h"
 
-static int	get_color(int iterations)
+void	design_coordenates(t_fractol *f)
 {
-	double	t;
-	int		r;
-	int		g;
-	int		b;
-
-	if (iterations == MAX_ITER)
-		return (0x000000);
-	t = (double)iterations / MAX_ITER;
-	r = (int)(9 * (1 - t) * t * t * t * 255);
-	g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
-	b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-	return ((r << 16) | (g << 8) | b);
+	if (f->type == J)
+	{
+		f->min_re = -2.0;
+		f->max_re = 2.0;
+		f->min_im = -2.0;
+		f->max_im = f->min_im + (f->max_re - f->min_re) * HEIGHT / WIDTH;
+	}
+	else
+	{
+		f->min_re = -2.0;
+		f->max_re = 1.0;
+		f->max_im = -1.5;
+		f->min_im = f->max_im + (f->max_re - f->min_re) * HEIGHT / WIDTH;
+	}
 }
 
-void	draw_fractal(t_mlx *mlx, t_fractol *f)
+static void	put_color_pos_imxel(t_fractol *f, int x, int y, int color)
 {
-	double	c_re;
-	double	c_im;
+	int	index;
+
+	index = x * 4 + y * WIDTH * 4;
+	f->data[index] = (unsigned char)(color & 0xFF);
+	f->data[index + 1] = (unsigned char)((color >> 8) & 0xFF);
+	f->data[index + 2] = (unsigned char)((color >> 16) & 0xFF);
+	f->data[index + 3] = (unsigned char)((color >> 24) & 0xFF);
+}
+
+static int	switch_fractal(t_fractol *f, double pos_re, double pos_im)
+{
+	int	num_iter;
+
+	if (f->type == M)
+		num_iter = mandelbrot(pos_re, pos_im);
+	else if (f->type == J)
+		num_iter = julia(f, pos_re, pos_im);
+	return (num_iter);
+}
+
+void	draw_fractal(t_fractol *f)
+{
 	int		x;
 	int		y;
-	int		pixel;
+	double	pos_re;
+	double	pos_im;
+	int		num_iter;
 
+	mlx_clear_window(f->mlx_ptr, f->win_ptr);
 	y = -1;
 	while (++y < HEIGHT)
 	{
 		x = -1;
 		while (++x < WIDTH)
 		{
-			c_re = f->min_re + x * f->re_factor;
-			c_im = f->max_im - y * f->im_factor;
-			if (f->type == 'M')
-				f->color = mandelbrot(c_re, c_im);
-			else if (f->type == 'J')
-				f->color = julia(c_re, c_im, f->julia_re, f->julia_im);
-			pixel = (x * mlx->bpp / 8) + (y * mlx->sline);
-			if (f->color == MAX_ITER)
-				*(int *)(mlx->data + pixel) = 0x000000;
-			else
-				*(int *)(mlx->data + pixel) = get_color(f->color);
+			pos_re = f->min_re + (double)x * (f->max_re - f->min_re) / WIDTH;
+			pos_im = f->max_im + (double)y * (f->min_im - f->max_im) / HEIGHT;
+			num_iter = switch_fractal(f, pos_re, pos_im);
+			put_color_pos_imxel(f, x, y, f->palette[num_iter]);
 		}
 	}
-}
-
-void	update_fractal(t_mlx *mlx, t_fractol *fractal)
-{
-	mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-	draw_fractal(mlx, fractal);
-	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_ptr, 0, 0);
+	mlx_put_image_to_window(f->mlx_ptr, f->win_ptr, f->img_ptr, 0, 0);
 }
